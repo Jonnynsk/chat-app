2de773df404513706d70a67e2c30c9593067e486
Evgeniy Fayzrakhmanov <Jonnynsk@mail.ru>
2021-10-19 20:54:36 +0700

    feat: organized folders

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,9 @@
-import React, { FC } from 'react'
+import { FC } from 'react'
 
 import './App.scss'
 
 const App: FC = () => {
-	return (
-		<div className='App'>
-			<header className='App-header'>
-				<p>
-					Edit <code>src/App.tsx</code> and save to reload.
-				</p>
-				<a
-					className='App-link'
-					href='https://reactjs.org'
-					target='_blank'
-					rel='noopener noreferrer'
-				>
-					Learn React
-				</a>
-			</header>
-		</div>
-	)
+	return <div className='App'></div>
 }
 
 export default App
